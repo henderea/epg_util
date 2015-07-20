d7b00da4692ae89301dabd816dbe050da8a0e9f1
Eric Henderson <henderea@gmail.com>
2015-07-20 16:09:43 -0400

    fix icon module

--- lib/epg/plugin/icon.plugin.rb
+++ lib/epg/plugin/icon.plugin.rb
@@ -3,14 +3,14 @@ require 'fastimage_resize'
 require 'everyday-plugins'
 include EverydayPlugins
 module EpgUtil
-  class Archive
+  class Icon
     extend Plugin
 
     register(:command, id: :path_icon, parent: :path, name: 'icon', short_desc: 'icon', desc: 'print out the path of the file for the icon utility module') { puts __FILE__ }
 
     register :command, id: :icon, parent: nil, name: 'icon', short_desc: 'icon SUBCOMMAND ARGS...', desc: 'run some icon utilities'
 
-    register(:command, id: :icon_generator, name: 'generator', aliases: %w(gen icns), short_desc: 'generator [input_file="Icon.png" [output_file="Icon.icns"]]', desc: 'generate a multi-resolution icns file') { |input_file = 'Icon.png', output_file = 'Icon.icns'|
+    register(:command, id: :icon_generator, parent: :icon, name: 'generator', aliases: %w(gen icns), short_desc: 'generator [input_file="Icon.png" [output_file="Icon.icns"]]', desc: 'generate a multi-resolution icns file') { |input_file = 'Icon.png', output_file = 'Icon.icns'|
       sizes = [512, 256, 128, 32, 16]
       folder = '/tmp/Icon.iconset'
       FileUtils.mkdir_p(folder)
@@ -46,12 +46,12 @@ module EpgUtil
 
       puts "Your file is called: #{output_file}\nDone!"
     }
-  end
 
-  register(:command, id: :icon_favicon, name: 'favicon', aliases: %w(fav gen_fav gen_favicon), short_desc: 'favicon [input_file="Icon.png" [output_file="favicon.ico"]]', desc: 'generate a multi-resolution favicon') { |input_file = 'Icon.png', output_file = 'favicon.ico'|
-    sizes = [16, 32, 48, 57, 64, 72, 96, 110, 114, 120, 128, 144, 152, 180, 192, 195, 228, 310]
-    puts 'Generating favicon file'
-    `convert #{input_file} -background transparent #{sizes.map { |s| "\\( -clone 0 -resize #{s}x#{s} \\)" }.join(' ')} -delete 0 -alpha on -depth 32 #{output_file}`
-    puts "Your file is called: #{output_file}\nDone!"
-  }
+    register(:command, id: :icon_favicon, parent: :icon, name: 'favicon', aliases: %w(fav gen_fav gen_favicon), short_desc: 'favicon [input_file="Icon.png" [output_file="favicon.ico"]]', desc: 'generate a multi-resolution favicon') { |input_file = 'Icon.png', output_file = 'favicon.ico'|
+      sizes = [16, 32, 48, 57, 64, 72, 96, 110, 114, 120, 128, 144, 152, 180, 192, 195, 228, 310]
+      puts 'Generating favicon file'
+      `convert #{input_file} -background transparent #{sizes.map { |s| "\\( -clone 0 -resize #{s}x#{s} \\)" }.join(' ')} -delete 0 -alpha on -depth 32 #{output_file}`
+      puts "Your file is called: #{output_file}\nDone!"
+    }
+  end
 end
